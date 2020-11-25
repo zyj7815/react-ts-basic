39ed9188d3ff9696f5eceff21dceb59ae169b8f3
zyj <zhuyuanjie1992@126.com>
2020-11-25 18:31:49 +0800

    update: 调整时间轴

--- src/pages/animal-wrapper/record/index.tsx
+++ src/pages/animal-wrapper/record/index.tsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import AwePage from '@/components/awe-page'
+import './index.less'
 
 const AnimalRecord: React.FC = props => {
-    return <div>AnimalRecord</div>
+    return (
+        <AwePage
+            bgColor={true}
+            className="animal-record-wrapper"
+            mainClass="beauty-radius beauty-shadow"
+        >
+            <ul className="animal-record-main">
+                {[1, 2, 3, 4, 5, 6, 7, 11, 12, 13, 14, 16, 22].map(value => (
+                    <li className="animal-record__item" key={value}>
+                        <div className="animal-record__item--title">title</div>
+                        <article className="animal-record__item--remark">
+                            <p>
+                                备注信息：虚拟虚拟低哦骗人虚拟虚拟低哦骗人虚拟虚拟低哦骗人虚拟虚拟低哦骗人虚拟虚拟低哦骗人
+                            </p>
+                        </article>
+                    </li>
+                ))}
+            </ul>
+        </AwePage>
+    )
 }
 
 export default AnimalRecord
